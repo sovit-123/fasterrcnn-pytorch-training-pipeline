7ce276691c58a5ac8f6bca27f1da2d6977aa999e
sovit-123 <sovitrath6@gmail.com>
2022-01-20 20:36:34 +0530

    Added EfficientNet models

---
+++ models/fasterrcnn_efficientnet_b4.py
@@ -0,0 +1,44 @@
+"""
+Faster RCNN model with the EfficientNetB4 backbone.
+"""
+
+import torchvision
+
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.rpn import AnchorGenerator
+
+def create_model(num_classes):
+    # Load the pretrained EfficientNetB0 large features.
+    backbone = torchvision.models.efficientnet_b4(pretrained=True).features
+    print(backbone)
+
+    # We need the output channels of the last convolutional layers from
+    # the features for the Faster RCNN model.
+    backbone.out_channels = 1792
+
+    # Generate anchors using the RPN. Here, we are using 5x3 anchors.
+    # Meaning, anchors with 5 different sizes and 3 different aspect 
+    # ratios.
+    anchor_generator = AnchorGenerator(
+        sizes=((32, 64, 128, 256, 512),),
+        aspect_ratios=((0.5, 1.0, 2.0),)
+    )
+
+    # Feature maps to perform RoI cropping.
+    # If backbone returns a Tensor, `featmap_names` is expected to
+    # be [0]. We can choose which feature maps to use.
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(
+        featmap_names=['0'],
+        output_size=7,
+        sampling_ratio=2
+    )
+
+    # Final Faster RCNN model.
+    model = FasterRCNN(
+        backbone=backbone,
+        num_classes=num_classes,
+        rpn_anchor_generator=anchor_generator,
+        box_roi_pool=roi_pooler
+    )
+
+    return model
