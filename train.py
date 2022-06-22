7da7f548f2e59c6ff53dae0e52ee953a2d5fc45e
sovit-123 <sovitrath6@gmail.com>
2022-06-22 19:08:20 +0530

    Removed weight decay from SDD optimizer. Tends to give slightly better results

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ def main(args):
     # Get the model parameters.
     params = [p for p in model.parameters() if p.requires_grad]
     # Define the optimizer.
-    optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, weight_decay=0.0005)
+    optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, nesterov=True)
     # optimizer = torch.optim.AdamW(params, lr=0.0001, weight_decay=0.0005)
 
     if args['cosine_annealing']:
