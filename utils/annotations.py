9892a401f4e8e2da1cbb938669fc88853710abcc
sovit-123 <sovitrath6@gmail.com>
2022-09-23 06:42:57 +0530

    Dynamic annotation

--- utils/annotations.py
+++ utils/annotations.py
@@ -12,17 +12,40 @@ def inference_annotations(
     draw_boxes = boxes.copy()
     # Get all the predicited class names.
     pred_classes = [classes[i] for i in outputs[0]['labels'].cpu().numpy()]
+
+    lw = max(round(sum(orig_image.shape) / 2 * 0.003), 2)  # Line width.
+    tf = max(lw - 1, 1) # Font thickness.
     
     # Draw the bounding boxes and write the class name on top of it.
     for j, box in enumerate(draw_boxes):
+        p1, p2 = (int(box[0]), int(box[1])), (int(box[2]), int(box[3]))
         class_name = pred_classes[j]
         color = colors[classes.index(class_name)]
-        cv2.rectangle(orig_image,
-                    (int(box[0]), int(box[1])),
-                    (int(box[2]), int(box[3])),
-                    color, 2)
-        cv2.putText(orig_image, class_name, 
-                    (int(box[0]), int(box[1]-5)),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 
-                    2, lineType=cv2.LINE_AA)
+        cv2.rectangle(
+            orig_image,
+            p1, p2,
+            color=color, 
+            thickness=lw,
+            lineType=cv2.LINE_AA
+        )
+        # For filled rectangle.
+        w, h = cv2.getTextSize(
+            class_name, 
+            0, 
+            fontScale=lw / 3, 
+            thickness=tf
+        )[0]  # text width, height
+        outside = p1[1] - h >= 3
+        p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
+        cv2.rectangle(orig_image, p1, p2, color, -1, cv2.LINE_AA)  
+        cv2.putText(
+            orig_image, 
+            class_name, 
+            (p1[0], p1[1] - 5),
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            fontScale=lw/3, 
+            color=(255, 255, 255), 
+            thickness=tf, 
+            lineType=cv2.LINE_AA
+        )
     return orig_image
